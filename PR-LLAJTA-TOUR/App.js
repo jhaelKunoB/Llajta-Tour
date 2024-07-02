0cdc77b0c2b0152c25e7e23884d4b60389541202
iriarteAbiel <141873131+iriarteAbiel@users.noreply.github.com>
2024-07-02 15:06:09 -0400

    Corrección de estilos.
    Se crearon archivos aparte para manejar los estilos.

---
+++ PR-LLAJTA-TOUR/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, SafeAreaView } from 'react-native';
+
+import Navigation from './src/presentation/navigator/navigation';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navigation/>
+      <StatusBar style='auto' />
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
