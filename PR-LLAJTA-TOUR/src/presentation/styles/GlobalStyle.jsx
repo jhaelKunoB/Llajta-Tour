85abfbb3297fe5107c8a32305d9f34f72ee1e6a1
Jhael Kuno <kbj5001497@est.univalle.edu>
2024-10-15 10:05:21 -0400

    Se Agrego la carpeta GlobalStyle

---
+++ PR-LLAJTA-TOUR/src/presentation/styles/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+const colors = {
+    violeta: "#A61B4E",
+    violeta2: "#590F32",
+    violeta3: "#8C4F70",
+    violeta4: "#FDFDFE",
+}
+
+const colorText = {
+    black: "#1B2021"
+}
+
+export {colorText,colors}
